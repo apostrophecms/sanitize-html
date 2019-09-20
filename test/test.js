a338283a991bbeb0520bc1f8f053c0dce2b2a5d0
Tom Boutell <tom@apostrophecms.com>
2019-09-20 09:33:59 -0400

    Revert "Escape disallowed tags"

--- test/test.js
+++ test/test.js
@@ -19,9 +19,6 @@ describe('sanitizeHtml', function() {
   it('should reject markup not whitelisted without destroying its text', function() {
     assert.equal(sanitizeHtml('<div><wiggly>Hello</wiggly></div>'), '<div>Hello</div>');
   });
-  it('should escape markup not whitelisted', function() {
-    assert.equal(sanitizeHtml('<div><wiggly>Hello</wiggly></div>', { escapeDisallowedTags: true }), '<div>&lt;wiggly&gt;Hello&lt;/wiggly&gt;</div>');
-  });
   it('should accept a custom list of allowed tags', function() {
     assert.equal(sanitizeHtml('<blue><red><green>Cheese</green></red></blue>', { allowedTags: [ 'blue', 'green' ] }), '<blue><green>Cheese</green></blue>');
   });
@@ -822,22 +819,4 @@ describe('sanitizeHtml', function() {
   //       }
   //     }), '<img src="&lt;0&amp;0;0.2&amp;" />');
   // });
-  it('should escape markup not whitelisted and all its children', function() {
-    assert.equal(
-        sanitizeHtml('<div><wiggly>Hello<p>World</p></wiggly></div>', { escapeDisallowedTags: true }),
-        '<div>&lt;wiggly&gt;Hello&lt;p&gt;World&lt;/p&gt;&lt;/wiggly&gt;</div>'
-    );
-  });
-  it('should escape markup even when deocdeEntities is false', function() {
-    assert.equal(
-        sanitizeHtml('<wiggly>Hello</wiggly>', { escapeDisallowedTags: true, parser: { decodeEntities: false } }),
-        '&lt;wiggly&gt;Hello&lt;/wiggly&gt;'
-    );
-  });
-  it('should escape markup not whitelisted even with not whitelisted children', function() {
-    assert.equal(
-        sanitizeHtml('<div><wiggly>Hello<p>World</p><tiggly>JS</tiggly></wiggly></div>', { escapeDisallowedTags: true }),
-        '<div>&lt;wiggly&gt;Hello&lt;p&gt;World&lt;/p&gt;&lt;tiggly&gt;JS&lt;/tiggly&gt;&lt;/wiggly&gt;</div>'
-    );
-  });
 });
