50d309f0f840160592fd3659b81cd8ba0f80edbf
Alex Bea <abea@mailbox.org>
2020-07-31 08:39:39 -0500

    Removes nl tag from defaults

--- index.js
+++ index.js
@@ -656,9 +656,11 @@ const htmlParserDefaults = {
   decodeEntities: true
 };
 sanitizeHtml.defaults = {
-  allowedTags: ['h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
-    'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'abbr', 'code', 'hr', 'br', 'div',
-    'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre'],
+  allowedTags: [
+    'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol', 'li',
+    'b', 'i', 'strong', 'em', 'strike', 'abbr', 'code', 'hr', 'br', 'div',
+    'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre'
+  ],
   disallowedTagsMode: 'discard',
   allowedAttributes: {
     a: ['href', 'name', 'target'],
