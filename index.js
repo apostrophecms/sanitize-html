cb11679aa30b3f2109c14a31d8a5f37e4ac74aa6
Alex Bea <abea@mailbox.org>
2020-07-31 11:59:34 -0500

    expands default allowedTags

--- index.js
+++ index.js
@@ -657,10 +657,24 @@ const htmlParserDefaults = {
 };
 sanitizeHtml.defaults = {
   allowedTags: [
-    'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
-    'div', 'blockquote', 'p', 'ul', 'ol', 'li', 'a',
-    'b', 'i', 'strong', 'em', 'strike', 'abbr', 'code', 'hr', 'br',
-    'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre'
+    // Sections derrived from MDN element categories and limited to the more
+    // benign categories.
+    // https://developer.mozilla.org/en-US/docs/Web/HTML/Element
+    // Content sectioning
+    'address', 'article', 'aside', 'footer', 'header',
+    'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hgroup',
+    'main', 'nav', 'section',
+    // Text content
+    'blockquote', 'dd', 'div', 'dl', 'dt', 'figcaption', 'figure',
+    'hr', 'li', 'main', 'ol', 'p', 'pre', 'ul',
+    // Inline text semantics
+    'a', 'abbr', 'b', 'bdi', 'bdo', 'br', 'cite', 'code', 'data', 'dfn',
+    'em', 'i', 'kbd', 'mark', 'q',
+    'rb', 'rp', 'rt', 'rtc', 'ruby',
+    's', 'samp', 'small', 'span', 'strong', 'sub', 'time', 'u', 'var', 'wbr',
+    // Table content
+    'caption', 'col', 'colgroup', 'table', 'tbody', 'td', 'tfoot', 'th',
+    'thead', 'tr'
   ],
   disallowedTagsMode: 'discard',
   allowedAttributes: {
